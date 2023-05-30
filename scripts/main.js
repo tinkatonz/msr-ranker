effff6ddf5d32e4a6a939dbc4ff434cad062d86b
feven <122063026+tinkatonz@users.noreply.github.com>
2023-05-30 22:01:39 +0200

    updated "Get Link" option

--- scripts/main.js
+++ scripts/main.js
@@ -334,7 +334,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://produce48.github.io/";
+const currentURL = "https://tinkatonz.github.io/msr-ranker/";
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
